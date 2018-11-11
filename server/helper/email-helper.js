00866ef1d0f51c33bfa97cddf8f08eca5eb99789
SamaliLiyanage <samali.liyanage93@gmail.com>
2018-11-11 14:10:39 +0530

    Change for testing

--- server/helper/email-helper.js
+++ server/helper/email-helper.js
@@ -10,7 +10,7 @@ var transporter = nodemailer.createTransport({
     },
     auth: {
         user: 'fao.testbed@gmail.com',
-        pass: process.env.EMAIL_PASSWORD
+        pass: 'Rand0mm4il!!!'
     }
 });
 
