a958edae0f15bcde9f96fdba126f4d7e2480ebc4
SamaliLiyanage <samali.liyanage93@gmail.com>
2018-11-20 00:24:52 +0530

    Login bug fixed

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
-import { Form, Col, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Form, Col, Button, FormGroup, FormControl, ControlLabel, Alert } from "react-bootstrap";
 import axios from 'axios';
+
+function LoginError(props){
+  if(props.display){
+    return (
+      <Col smOffset={4}>
+        <Alert className='col-sm-6' bsStyle="danger" >Invalid username or password...</Alert>
+      </Col>
+    );
+  } else {
+    return null;
+  }
+} 
 
 export default class Login extends Component {
   constructor(props) {
@@ -19,24 +31,24 @@ export default class Login extends Component {
   componentDidMount() {
     const authenticate = this.props;
     axios.get('/loggedin')
-    .then(function (res){
-      if(res.data=="") {
-        authenticate.userHasAuthenticated(false, null, null);
-        authenticate.history.push('/login');
-      } else {
-        console.log(res.data.Username, res.data.Role)
-        authenticate.userHasAuthenticated(true, res.data.Username, res.data.Role);
-        if(res.data.Role===1) {
-          authenticate.history.push('/viewusers');
-        } else if (res.data.Role===4) {
-          authenticate.history.push('/requesttrip');
-        } else if (res.data.Role===2) {
-          authenticate.history.push('/viewtrips');
-        } else if (res.data.Role===5) {
-          authenticate.history.push('/viewfrequests');
+      .then(function (res) {
+        if (res.data == "") {
+          authenticate.userHasAuthenticated(false, null, null);
+          authenticate.history.push('/login');
+        } else {
+          console.log(res.data.Username, res.data.Role)
+          authenticate.userHasAuthenticated(true, res.data.Username, res.data.Role);
+          if (res.data.Role === 1) {
+            authenticate.history.push('/viewusers');
+          } else if (res.data.Role === 4) {
+            authenticate.history.push('/requesttrip');
+          } else if (res.data.Role === 2) {
+            authenticate.history.push('/viewtrips');
+          } else if (res.data.Role === 5) {
+            authenticate.history.push('/viewfrequests');
+          }
         }
-      }
-    })
+      })
   }
 
   validateForm() {
@@ -54,24 +66,24 @@ export default class Login extends Component {
     const authenticate = this.props;
 
     axios.post('/login', {
-        username: this.state.username,
-        password: this.state.password,
-      })
+      username: this.state.username,
+      password: this.state.password,
+    })
       .then(function (res) {
         console.log(res);
         var data = res.data.result;
-        if(res.data.status === "success"){
+        if (res.data.status === "success") {
           authenticate.userHasAuthenticated(true, data[0].Username, data[0].Role);
-          if((data[0].Role)===1) {
+          if ((data[0].Role) === 1) {
             authenticate.history.push('/viewusers');
-          }else if((data[0].Role)===4) {
+          } else if ((data[0].Role) === 4) {
             authenticate.history.push('/requesttrip');
-          }else if((data[0].Role)===2) {
+          } else if ((data[0].Role) === 2) {
             authenticate.history.push('/viewtrips');
-          } if((data[0].Role)===5) {
+          } if ((data[0].Role) === 5) {
             authenticate.history.push('/viewfrequests');
-          } 
-        } else if(res.data.status === "fail") {
+          }
+        } else if (res.data.status === "fail") {
           console.log("In here");
           this.setState({
             username: "",
@@ -79,33 +91,33 @@ export default class Login extends Component {
             loginFail: true
           });
           authenticate.userHasAuthenticated(false, null, null);
-        } else{
+        } else {
           console.log("In here");
           this.setState({
             username: "",
             password: "",
-            loginFail: true
+            loginFail: true,
+            message: res.data.info
           });
           authenticate.userHasAuthenticated(false, null, null);
-          //authenticate.history.push('/login');
         }
       })
-      // .catch(function (error) {
-      //   console.log("Response error::::", error);
-      //   this.setState({
-      //     username: "",
-      //     password: "",
-      //     loginFail: true
-      //   });
-      //   authenticate.userHasAuthenticated(false, null, null);
-      // })
+      .catch((error) => {
+        console.log("Response error::::", error);
+        this.setState({
+          username: "",
+          password: "",
+          loginFail: true
+        });
+        authenticate.userHasAuthenticated(false, null, null);
+      })
 
 
   }
 
   render() {
     return (
-      <Form horizontal onSubmit={this.handleSubmit}>
+        <Form horizontal onSubmit={this.handleSubmit}>
           <FormGroup controlId="username" >
             <Col sm={2} smOffset={2}><ControlLabel>Username</ControlLabel></Col>
             <Col sm={4}>
@@ -119,6 +131,7 @@ export default class Login extends Component {
             </Col>
           </FormGroup>
           <Button disabled={!this.validateForm()} type="submit"> Login </Button>
+          <LoginError display={this.state.loginFail} />
         </Form>
     );
   }
