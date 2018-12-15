91be492f70bac080bb28aa8c915c87b2ad324cb1
SamaliLiyanage <samali.liyanage93@gmail.com>
2018-12-15 19:04:36 +0530

    Fix to end time generator

--- server/helper/time-helper.js
+++ server/helper/time-helper.js
@@ -2,7 +2,7 @@ module.exports.getEndTime = function (startTime, durationHrs, durationMins) {
     return new Promise((resolve, reject) => {
         var endTime;
         var endHours = parseInt(startTime.slice(0, 2), 10) + parseInt(durationHrs, 10);
-        var endMins = parseInt(startTime.slice(-2), 10) + parseInt(durationMins, 10);
+        var endMins = parseInt(startTime.slice(), 10) + parseInt(durationMins, 10);
         
         if (endHours.toString().length === 1) {
             endTime = '0' + endHours + ':';
